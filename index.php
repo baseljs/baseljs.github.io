616e0be8d0e4f9cf74f8be70efc2a482f2c3aa70
Jayr Alencar <jayralencarpereira@gmail.com>
2016-04-20 10:26:19 -0300

    header

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>BASEL - Bootstrap, Angular, SQLite and Electron</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.5 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="dist/css/style.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+    folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+      </head>
+      <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+      <body class="hold-transition skin-purple layout-top-nav">
+        <div class="wrapper">
+
+          <header class="main-header">
+            <nav class="navbar navbar-static-top">
+              <div class="container">
+                <div class="navbar-header">
+                  <a href="index.html" class="navbar-brand" style="padding-top: 10px"><img class="img-responsive" src="dist/img/logo.fw.png"></a>
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                    <i class="fa fa-bars"></i>
+                  </button>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse pull-right" id="navbar-collapse">
+                  <ul class="nav navbar-nav" style="margin-top:10px;margin-bottom:10px">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Documentation</a></li>
+                    <li><a href="#">Releases Notes</a></li>
+                    <li><a href="#">Contact</a></li>
+                  </ul>
+                  <!-- <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                      <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
+                    </div>
+                  </form> -->
+                </div><!-- /.navbar-collapse -->
+
+              </div><!-- /.container-fluid -->
+            </nav>
+          </header>
+          <!-- Full Width Column -->
+          <div class="content-wrapper">
+            <div class="container">
+              <!-- Content Header (Page header) -->
+              <section class="content-header">
+              </section>
+
+              <!-- Main content -->
+              <section class="content">
+              </section><!-- /.content -->
+            </div><!-- /.container -->
+          </div><!-- /.content-wrapper -->
+          <footer class="main-footer">
+            <div class="container">
+              <div class="pull-right hidden-xs">
+                <b>Version</b> 0.0.1
+              </div>
+              <strong>Copyright &copy; 2016 <a href="http://clubedosgeeks.com.br">Clube dos Geeks</a>.</strong> All rights reserved.
+            </div><!-- /.container -->
+          </footer>
+        </div><!-- ./wrapper -->
+
+        <!-- jQuery 2.1.4 -->
+        <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+        <!-- Bootstrap 3.3.5 -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="dist/js/app.min.js"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="dist/js/demo.js"></script>
+      </body>
+      </html>
