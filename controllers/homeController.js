e0400843153045dd261fdf8e6c21f59d862fceff
Jayr Alencar <jayralencarpereira@gmail.com>
2016-04-20 14:43:47 -0300

    stands

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,7 @@ app.controller("homeController", function($scope, $location, githubService){
 	$scope.init = function(){
 		githubService.repo().success(function(data){
 			$scope.repo = data;
-			$scope.repo.latest_commit = (new Date(data.pushed_at)).toLocaleFormat('%d %b %Y');
+			$scope.repo.latest_commit = (new Date(data.pushed_at)).toString().substring(4,15);
 		});
 		githubService.releases().success(function(data){
 			$scope.releases = data;
