68a626ac78b217ad0a38df4509cd0a81b7dc87bb
JayrAlencar <jayralencarpereira@gmail.com>
2016-04-21 17:16:32 -0300

    navbar fixed, social links, documentation coming

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,5 +4,7 @@ app.config(function($routeProvider){
 	$routeProvider.when("/", {
 		templateUrl : "views/home.html",
 		controller: "homeController"
-	})
+	}).when('/documentation',{
+		templateUrl : "views/documentation.html"
+	});
 });
