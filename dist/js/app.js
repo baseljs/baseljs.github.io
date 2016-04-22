2276156ce9b502013d58c6fa0db6127c709c8be1
Jayr Alencar <jayralencarpereira@gmail.com>
2016-04-22 10:07:57 -0300

    Installation

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,11 +5,17 @@ app.config(function($routeProvider){
 		templateUrl : "views/home.html",
 		controller: "homeController"
 	})
+	.when('/documentation',{
+		templateUrl : "views/documentation.html"
+	})
 	.when('/prerequisites',{
 		templateUrl : "views/prerequisites.html"
 	})
 	.when('/plataforms',{
 		templateUrl : "views/plataforms.html"
+	})
+	.when('/instalation',{
+		templateUrl : "views/instalation.html"
 	})
 	;
 });
