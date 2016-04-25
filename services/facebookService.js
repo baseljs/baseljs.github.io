6ccf4574735aabac8605424e38cd1c8a1950014a
Jayr Alencar <jayralencarpereira@gmail.com>
2016-04-25 10:58:58 -0300

    Facebook tests

--- services/facebookService.js
+++ services/facebookService.js
@@ -2,7 +2,7 @@
 app.factory("facebookService", function($http, $q){
 	var get = function(){
 		var deferred = $q.defer();
-		FB.api('/baselfwk/likes', function(response) {
+		FB.api('/baselfwk', function(response) {
 			if (!response || response.error) {
 				deferred.reject('Error occured');
 			} else {
